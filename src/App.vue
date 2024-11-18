aa0e70ede64a401ca2fbf815d01a4c38bf5fe327
KaterinaKlachun <kataklachun@mail.ru>
2024-11-18 22:42:08 +1000

    update

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@ export default {
 
 </script>
 
-<style lang="scss">
+<style lang="css">
 
 @font-face {
   font-family: "bold";
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 const { defineConfig } = require('@vue/cli-service');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const path = require('path');
+const glob = require('glob-all');
 
 module.exports = defineConfig({
   publicPath: '/reklempub/',
@@ -10,6 +13,9 @@ module.exports = defineConfig({
     const imageminMozjpeg = await import('imagemin-mozjpeg');
 
     return {
+      optimization: {
+        minimize: true, // Минификация JS и CSS
+      },
       plugins: [
         new webpack.DefinePlugin({
           __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true),
@@ -23,7 +29,18 @@ module.exports = defineConfig({
             },
           },
         }),
+        new PurgeCSSPlugin({
+          paths: glob.sync([
+            path.join(__dirname, './src/**/*.vue'), // Поиск используемых классов в .vue файлах
+            path.join(__dirname, './public/index.html'), // и в HTML
+          ]),
+          safelist: ['safelist-class'], // Исключите классы, которые не должны удаляться
+        }),
       ],
     };
+  },
+  css: {
+    extract: true, // Вынесение CSS в отдельные файлы
+    sourceMap: false, // Отключение карт кода для продакшна
   },
 });
